22e2fc61827b732e213f3e612d179444afce8029
Tom Trebisky <tom@mmto.org>
2018-08-25 17:38:06 -0700

    Add code to see if extra cores start.

--- Spl_blink/boot.c
+++ Spl_blink/boot.c
@@ -36,30 +36,83 @@ struct gpio_regs {
 
 #define GPIOB_BASE	((struct gpio_regs *) 0xC001B000)
 
-#define DELAY_COUNT 400000
+#define LONG  800000
+#define MEDIUM  200000
+#define SHORT 100000
 
 #define LED_BIT	(1<<12)
+
+static void
+delay ( int count )
+{
+	while ( count )
+	    count--;
+}
+
+static void
+pulse ( void )
+{
+	struct gpio_regs *gp = GPIOB_BASE;
+
+	gp->out = LED_BIT;
+	delay ( MEDIUM );
+	gp->out = 0;
+	delay ( SHORT );
+	gp->out = LED_BIT;
+}
+
+static void
+pulses ( int num )
+{
+	while ( num-- )
+	    pulse ();
+}
+
+/* This business is an experiment to find out if other cores
+ * are starting (they are not, it turns out).
+ * There is code in start.S (from Metro94) that looks like
+ * it will handle other cores, and will call "boot_slave"
+ * for any cores that start.  I made a simple change to start.S
+ * to pass the cpu_id number (1-7) to boot_slave.
+ * cpu_id == 0 gets routed to boot_master.
+ * I use an array to avoid race conditions that might
+ * arise if several cores were starting at once.
+ * The result will be that this code will blink a number
+ * of times that indicates the number of cores that start.
+ * It blinks only once.
+ */
+
+#define NUM_CPU	8
+int cpu_active[NUM_CPU] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
 void
 boot_master (void)
 {
 	struct gpio_regs *gp = GPIOB_BASE;
+	int i;
 	int count;
-	int led;
 
 	gp->alt0 &= ~(3<<24);
 	gp->alt0 |= 2<<24;
 	gp->oe = LED_BIT;
 
-	led = 0;
+	gp->out = LED_BIT;	/* off */
+
 	for ( ;; ) {
-	    for ( count=DELAY_COUNT; count; count-- )
-		;
-	    gp->out = led;
-	    led ^= LED_BIT;
+	    delay ( LONG );
+	    count = 1;
+	    for ( i=0; i<NUM_CPU; i++ )
+		if ( cpu_active[i] )
+		    count++;
+
+	    pulses ( count );
 	}
 }
 
-void boot_slave(void) { }
+void
+boot_slave ( int who )
+{
+	cpu_active[who]++;
+}
 
 /* THE END */
